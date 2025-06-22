fa565f574287a4210c2d58eb476f95bcf175f1c0
griffsauce123 <griffinadelmann@gmail.com>
2025-06-22 00:25:01 -0400

    templated

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+const arrow = document.querySelector(".scroll-arrow");
+
 window.addEventListener("scroll", () => {
-    const arrow = document.querySelector(".scroll-arrow");
-    if (window.scrollY > 30) {
+    if (window.scrollY > 80) {
         arrow.classList.add("hidden");
     } else {
         arrow.classList.remove("hidden");
     }
+});
+
+arrow.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.scrollBy({
+        top: 700,
+        left: 0,
+        behavior: "smooth"
+    });
 });
